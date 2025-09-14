a2af15157a58ef607e62dc5603df6402549f6cdb
navanken <afandinaufal67@gmail.com>
2025-09-14 09:55:54 +0700

    first commit

---
+++ src/pages/LandingPage/index.jsx
@@ -0,0 +1,12 @@
+const LandingPages = () => {
+  return (
+    <>
+      <div className="">
+        <h1>Eat Burgir</h1>
+        <p>Masih Dalam Tahap Pengembangan</p>
+      </div>
+    </>
+  );
+};
+
+export default LandingPages;
