e0af9aa81ea8456acf22419c1750a361c5971651
JendraBima <jendrasatria3@gmail.com>
2025-10-31 10:00:25 +0700

    fix deploy errro

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HeroUIProvider } from "@heroui/react";
-import App from "./app";
+import { ToastContainer} from 'react-toastify';
+import { RouterProvider } from "react-router";
+import { router } from "./lib/router";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HeroUIProvider>
-      <App />
+       <ToastContainer position="top-right" autoClose={3000} />
+      <RouterProvider router={router} />
     </HeroUIProvider>
   </StrictMode>
 );
